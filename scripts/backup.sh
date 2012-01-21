8a1bfcfdb0471ca84d6a075586e5f3bbb7617613
Justin Lucas <lukeas14@gmail.com>
2012-01-21 05:09:06 -0800

    Added app_images to backup script

--- scripts/backup.sh
+++ scripts/backup.sh
@@ -1,8 +1,19 @@
 #!/bin/bash
 
 DATE=$(date +%F)
-FILENAME=iwaat-db-backup_${DATE}.sql.gz
-touch $FILENAME
-mysqldump -u iwaat-backup --password="idk239i0ef@sf32" --replace --single-transaction iwaat | gzip > $FILENAME
-s3cmd put $FILENAME s3://iwaat-backup/$FILENAME
-rm $FILENAME
+S3_BUCKET=s3:\/\/iwaat-backup\/
+
+#backup database
+DB_FILENAME=iwaat-db-backup_${DATE}.sql.gz
+touch $DB_FILENAME
+mysqldump -u iwaat-backup --password="idk239i0ef@sf32" --replace --single-transaction iwaat | gzip > $DB_FILENAME
+s3cmd put $DB_FILENAME s3://iwaat-backup/$DB_FILENAME
+rm $DB_FILENAME
+
+#backup app images
+IMG_DIR=/var/www/iwaat/public_html/images/apps
+IMG_FILENAME=app-images_${DATE}.gz
+touch $IMG_FILENAME
+tar -cvzf $IMG_FILENAME $IMG_DIR
+s3cmd put $IMG_FILENAME s3://iwaat-backup/$IMG_FILENAME
+rm $IMG_FILENAME
