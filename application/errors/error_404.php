38fadc266803a8b01460a093b1311b0e0ab3ab3e
Justin Lucas <lukeas14@gmail.com>
2012-02-20 15:31:48 -0800

    Added new 404 page

--- application/errors/error_404.php
+++ application/errors/error_404.php
@@ -1,10 +1,10 @@
 <html>
 <head>
-<title>404 Page Not Found</title>
+<title>404 Page Notiuuu Found</title>
 <style type="text/css">
 
 body {
-background-color:	#fff;
+background:#EEE url('/images/background.png') repeat;
 margin:				40px;
 font-family:		Lucida Grande, Verdana, Sans-serif;
 font-size:			12px;
@@ -12,23 +12,31 @@ color:				#000;
 }
 
 #content  {
-border:				#999 1px solid;
+width:500px;
+margin:100px auto;
+border:				#BBB 6px solid;
 background-color:	#fff;
-padding:			20px 20px 12px 20px;
+padding:			10px;
 }
 
+img.iwaat_logo{
+	margin:0 0 0 80px;
+}
 h1 {
 font-weight:		normal;
-font-size:			14px;
+font-size:			20px;
 color:				#990000;
-margin:				0 0 4px 0;
+margin:				35px 0 35px 0;
 }
 </style>
 </head>
 <body>
 	<div id="content">
+		<img class="iwaat_logo" src="/images/iwaat_logo.png"/>
+		<br/>
 		<h1><?php echo $heading; ?></h1>
 		<?php echo $message; ?>
+		You can either <a href="javascript:history.go(-1)"/>Go Back</a> or <a href="http://www.iwaat.com/">Go to the Homepage</a>.
 	</div>
 </body>
 </html>
