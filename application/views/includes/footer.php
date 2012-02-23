84fc0f15646267dd5ef024b45fd383bbc4b0b7e0
Justin Lucas <lukeas14@gmail.com>
2012-02-22 22:10:39 -0800

    Added uservoice widget

--- application/views/includes/footer.php
+++ application/views/includes/footer.php
@@ -22,6 +22,15 @@
 		</div>
 	</div>
 	
+	<script type="text/javascript">
+  var uvOptions = {};
+  (function() {
+    var uv = document.createElement('script'); uv.type = 'text/javascript'; uv.async = true;
+    uv.src = ('https:' == document.location.protocol ? 'https://' : 'http://') + 'widget.uservoice.com/XsrFY2SjjjUiTcLBMP5DQ.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(uv, s);
+  })();
+</script>
+	
 	<!--Google Analytics -->
 	<script type="text/javascript">
 
