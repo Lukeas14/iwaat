f1acb33123b0114129b9b63218463077d601059e
Justin Lucas <lukeas14@gmail.com>
2012-03-03 17:53:28 -0800

    Completed contact us page

--- application/controllers/static_pages.php
+++ application/controllers/static_pages.php
@@ -17,11 +17,23 @@ class Static_pages extends MY_Controller {
 		$this->form_validation->set_rules('message', 'Message', 'trim|required');
 		
 		if($this->form_validation->run() === true){
+			$this->load->library('swift_email');
+			
 			$message = "
-			Name: " . $this->input->post('name') . "
-			Email: " . $this->input->post('email') . "
-			Message: " . $this->input->post('message') . "	
+			Name: " . $this->input->post('name') . "<br/>
+			Email: " . $this->input->post('email') . "<br/>
+			Message: " . $this->input->post('message') . "<br/>
 			";
+			$email_params = array(
+				'html'		=> $message,
+				'text'		=> $message,
+				'subject'	=> 'IWAAT.com - Contact Form',
+				'to'		=> array(ADMIN_EMAIL_ADDRESS => ADMIN_EMAIL_NAME),
+				'from'		=> array($this->input->post('email')  => $this->input->post('name'))
+			);
+			$this->swift_email->send_email($email_params);
+			
+			$this->data['notifications']['confirm'] = "Thanks for contacting us.  We'll be in touch.";
 		}
 		else{
 			if($this->form_validation->errors_exist()){
