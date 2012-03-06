147c8d3f793a645c3361c2b4788e223352c1f7dc
Justin Lucas <lukeas14@gmail.com>
2012-03-05 22:11:52 -0800

    Updated copy on about us page

--- application/views/about_us.php
+++ application/views/about_us.php
@@ -11,21 +11,22 @@
 		</p>
 		<p>
 			IWAAT.com is the side project of web developer and wannabe entrepreneur Justin Lucas.
-			What began as a weekend goal to index the <a href="http://www.crunchbase.com">Crunchbase</a> database of 
+			What began as a weekend goal to index the <a href="http://www.crunchbase.com">Crunchbase</a> database has slowly expanded into the site IWAAT.com is today.
+			
 		</p>
 	</div>
 	
 	<p>
 		I Want An App That... (IWAAT.com) is a web application discovery engine.
-		Our mission is to help you discover and research the online tools most relevant what you need.
+		Our mission is to help you discover and research apps that you may never come across otherwise.
 		At the same time, we hope to provide application creators with increased visibility to potential users.
 	</p>
 		
 	<p id="about_discover" class="about_condensed">
 		<span>Discover:</span> Over the past decade, the number of web applications has exploded as developers create online solutions for every imaginable niche.
-		However, the ability to find the exact application you need amongst the thousands in existence 
-		We've taken the first step to solving this problem by creating a search engine and directory backed by our curated database of web apps.
-		Search for applications by the problem you need to solve, browse by category, or find applications related to the ones you already use.
+		However, as that number grows, your ability to find the exact application you need amongst the thousands in existence is getting harder.
+		We've taken the first step to solving this problem by curating a database of web apps and then using it to build a search engine and directory.
+		Search for applications by a specific problem you're trying to solve, browse apps by category, or find apps related to the ones you already use.
 	</p>
 
 	<p id="about_research" class="about_condensed">
@@ -37,9 +38,9 @@
 	</p>
 	
 	<p id="about_technology" class="about_condensed">
-		<span>Technology:</span> Powering IWAAT combined to create an index for each application.  ind the scenes is a full blown web index and 's search engine and traction index is a 
-		Behind the scenes Throughout the day our servers are busy gathering data from across the web by crawling site content, retrieving each app's social media and blog communications, and parsing what others are saying within social networks and the tech media.
-		These bits and pieces of information are then thrown into an algorithm that creates the search and traction indexes for every application.
+		<span>Technology:</span> Powering IWAAT.com is a constantly updating web index. 
+		Throughout the day our servers are busy gathering data from across the web by crawling site content, retrieving each app's social and blog communications, and parsing what others say about them within social networks and the tech media.
+		These bits and pieces of information are then thrown into an algorithm that creates the search and traction indexes.
 	</p>
 	
 	<p>
@@ -48,7 +49,7 @@
 		Improving search result relevance is also a top priority as we continuously iterate on our data gathering and indexing techniques.
 		
 		<br/><br/>
-		Be the first to know about and gain access to new features by signing up to our newsletter from the <a href="/">homepage</a>.
+		Be the first to know about and gain access to new features by signing up for our private beta using the form below.
 	</p>
 
 </div>
