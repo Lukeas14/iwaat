6d32b8a2882e7c0789659f6b65d144ca17ea3d91
Justin Lucas <lukeas14@gmail.com>
2012-03-12 20:47:24 -0700

    Added logo links

---
+++ public_html/logos.php
@@ -0,0 +1,5 @@
+<a href="/images/logos/logo1.png">Logo #1</a><br/>
+<a href="/images/logos/logo2.jpg">Logo #2</a><br/>
+<a href="/images/logos/logo3.jpg">Logo #3</a><br/>
+<a href="/images/logos/logo4.png">Logo #4</a><br/>
+<a href="/images/logos/logo5.jpg">Logo #5</a><br/>
