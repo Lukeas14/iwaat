f7866a031ba8fcdebd1fb6043808d903eeecf73c
Justin Lucas <lukeas14@gmail.com>
2012-03-14 21:55:49 -0700

    Fixed backup images script

--- scripts/backup_images.sh
+++ scripts/backup_images.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+source /var/www/iwaat/scripts/config.sh
+
 #backup images
 IMG_DIR=/var/www/iwaat/public_html/images/apps/
 s3cmd sync /var/www/iwaat/public_html/images/apps/ $S3_BUCKET/images/apps/
+
+echo "Done."
