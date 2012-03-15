98a9b05b28fd32a1cbc95158b72e4d8bd168c0bd
Justin Lucas <lukeas14@gmail.com>
2012-03-14 21:50:20 -0700

    fixed db backup mysql connection

--- scripts/backup_db.sh
+++ scripts/backup_db.sh
@@ -5,7 +5,7 @@ source /var/www/iwaat/scripts/config.sh
 #backup database
 DB_FILENAME=iwaat-db-backup_${DATE}.sql.gz
 touch $DB_FILENAME
-mysqldump -u jliwaatwesley --password="eiof2!J@Issd$I6kfsd4" --replace --single-transaction iwaat | gzip > $DB_FILENAME
+mysqldump -u jliwaatwesley --password=eiof2\!J\@Issd\$I6kfsd4 --replace --single-transaction iwaat | gzip > $DB_FILENAME
 s3cmd put $DB_FILENAME $S3_BUCKET/database/$DB_FILENAME
 rm $DB_FILENAME
 
