f8c3f4d6efe9c899185551a79ae798f86b8ae63b
Justin Lucas <lukeas14@gmail.com>
2012-03-17 16:18:31 -0700

    Turned off errors in production

--- application/config/env_constants.php
+++ application/config/env_constants.php
@@ -58,8 +58,8 @@ switch(ENVIRONMENT){
 	default:
 		define('BASE_URL', 'http://www.iwaat.com');
 		
-		error_reporting(E_ALL);
-		ini_set('display_errors',1);
+		error_reporting(0);
+		ini_set('display_errors',0);
 		
 		define('DB_HOST', 'griffey');
 		define('DB_USER', 'jliwaatwesley');
