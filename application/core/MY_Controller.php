20d81e04f91338130145419a66367a4bcc24bcf3
Justin Lucas <lukeas14@gmail.com>
2012-05-26 20:17:28 -0700

    Added minify to combine css and js scripts

--- application/core/MY_Controller.php
+++ application/core/MY_Controller.php
@@ -2,8 +2,23 @@
 
 class MY_Controller extends CI_Controller {
 
-    function __construct()
+	public $css_files = array('style.css', 'jquery-ui-1.8.16.custom.css');
+
+	public $js_files = array('script.js', 'qtip.js', 'jquery-ui-1.8.16.custom.min.js');
+
+	public    function __construct()
     {
         parent::__construct();
     }
+
+    public function set_js($file_path)
+    {
+    	array_push($this->js_files, $file_path);
+    }
+
+    public function set_css($file_path)
+    {
+    	array_push($this->css_files, $file_path);
+    }
+
 }
