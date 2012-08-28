77751f697945286a23be78d74557fa349c45df2b
Justin Lucas <lukeas14@gmail.com>
2012-08-27 23:03:55 -0700

    Update to discussions

--- application/controllers/discussions.php
+++ application/controllers/discussions.php
@@ -5,6 +5,20 @@ class Discussions extends MY_Controller {
 	private $data = array();
 	
 	public function add_discussion(){
+		$this->load->helper(array('form', 'url'));
+
+		if($_SERVER['REQUEST_METHOD'] == 'POST'){
+			$this->load->library('form_validation');
+
+			$validation_rules = array(
+				array(
+					'field' => ''
+				)
+			);
+		}
+		else{
+
+		}
 		echo"add a discussion";
 		echo"<pre>";print_r($_POST);echo"</pre>";
 		exit();
